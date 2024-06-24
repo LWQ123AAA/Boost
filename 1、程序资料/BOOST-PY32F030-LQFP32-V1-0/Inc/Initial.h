59b225325983d7ca9c27e2206b036cfeef3bdd23
luoweiqiang <2324066901@qq.com>
2024-06-24 19:02:14 +0800

    新增：按键函数，滴答定时器函数

---
+++ 1、程序资料/BOOST-PY32F030-LQFP32-V1-0/Inc/Initial.h
@@ -0,0 +1,12 @@
+#ifndef _INITIAL_H_
+#define _INITIAL_H_
+#include "public.h"
+
+#define SYSTICK_F 8000 //  SYSTICK_F µÎ´ð¶¨Ê±Æ÷ÆµÂÊ£º 8000 = 8K = 125us
+
+
+void SystemClockConfig(void);
+void Systick_Initial(void);
+
+#endif
+
